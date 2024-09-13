c587d10507756a0cff8220a4a065c1c298695c6f
Dimanerter <erterujamp@gmail.com>
2024-09-13 10:29:44 +0200

    arguments and parameters

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def sey_hello():
-    print("hello")
+def print_max(a : int, b : int):
+    if a > b:
+        print(a, ' максимально ')
+    elif a == b:
+        print(a, ' равно с ', b)
+    else:
+        print(b, 'максимально')
 
-sey_hello()
 
-sey_hello()
+
+print_max(3,4)
+
+x = 5
+y = 7
+print_max(x, y)
