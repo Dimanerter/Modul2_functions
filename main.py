780534ae2656bde70c05a899b13fa2ddd800873b
Dimanerter <erterujamp@gmail.com>
2024-09-13 12:43:29 +0200

    immutable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(num: int) -> bool:
-    return num % 2 == 0
-check_even = is_even(4)
-print(check_even)
+def modify_string(original: str) -> str:
+    original = "chenge"
+    return original
+str_var = "orig"
+print(modify_string(str_var))
+print(str_var)
