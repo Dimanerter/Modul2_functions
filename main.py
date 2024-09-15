0238a89a61d6038b1349e308d97a1ba57e023a97
Dimanerter <erterujamp@gmail.com>
2024-09-15 21:28:02 +0200

    *args

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-def real_cost(base: int, discount: float = 0) -> float:
-    return base * (1 - discount)
+def print_all_args(*args):
+    for arg in args:
+        print(arg)
 
-price_bread = 15
-price_butter = 50
-price_sugar = 60
-
-current_price_bread = real_cost(price_bread)
-current_price_butter = real_cost(price_butter, 0.05)
-current_price_sugar = real_cost(price_sugar, 0.07)
-
-print(f'Нова вартість хліба: {current_price_bread}')
-print(f'Нова вартість масла: {current_price_butter}')
-print(f'Нова вартість цукру: {current_price_sugar}')
+print_all_args(1, "Hello", True)
