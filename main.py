fc9dd58893b9a84eaccaf6f4dec38e8151fa0698
Dimanerter <erterujamp@gmail.com>
2024-09-15 21:22:39 +0200

    base/discount

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-def greet(name, message = "Привет"):
-    print(f"{message}, {name}")
+def real_cost(base: int, discount: float = 0) -> float:
+    return base * (1 - discount)
 
-# використовує значення за замовчуванням для message
-greet("Олексій")  
+price_bread = 15
+price_butter = 50
+price_sugar = 60
 
-# передача власного значення для message
-greet("Марія", message="Добрий день")  
+current_price_bread = real_cost(price_bread)
+current_price_butter = real_cost(price_butter, 0.05)
+current_price_sugar = real_cost(price_sugar, 0.07)
+
+print(f'Нова вартість хліба: {current_price_bread}')
+print(f'Нова вартість масла: {current_price_butter}')
+print(f'Нова вартість цукру: {current_price_sugar}')
